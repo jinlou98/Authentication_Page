d4cc60941c7d959c01997565d83db2e4e717916b
Jin Lou <jinlou@bu.edu>
2021-05-27 15:17:59 -0400

    Store input in individual component.

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,39 +1,37 @@
-import React, {useContext} from "react";
-// useContext does the job of .consumer 
+import React, { useContext } from "react";
+// useContext does the job of .consumer
 
 import classes from "./Navigation.module.css";
 import AuthContext from "../../store/auth-context";
 
-const Navigation = (props) => {
-  const ctx= useContext(AuthContext);
+const Navigation = () => {
+  const ctx = useContext(AuthContext);
   return (
-    // with consumer, we listen 
+    // with consumer, we listen
     // <AuthContext.Consumer>
 
-          <nav className={classes.nav}>
-        <ul>
-          {ctx.isLoggedIn && (
-            <li>
-              <a href="/">Users</a>
-            </li>
-          )}
-          {ctx.isLoggedIn && (
-            <li>
-              <a href="/">Admin</a>
-            </li>
-          )}
-          {ctx.isLoggedIn && (
-            <li>
-              <button onClick={ctx.onLogout}>Logout</button>
-            </li>
-          )}
-        </ul>
-      </nav>
-        )
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={ctx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 
-      
-    // </AuthContext.Consumer>
-
+  // </AuthContext.Consumer>
 };
 
 export default Navigation;
